a97bf771b03adb239a14350872ff34081eb7ef0c
EsterPuzzangaro <esterpuzzangaro@gmail.com>
2023-06-05 13:07:21 +0200

    file completi
    file completi

---
+++ js/fattoriale.js
@@ -0,0 +1,22 @@
+function calcolaFattoriale ($numero) {
+ return $numero *5 *4 *3 *2 *1
+}
+
+$numero = 6;
+$fattoriale = calcolaFattoriale ($numero)
+echo "Il fattoriale di $numero è $fattoriale.";
+
+
+
+
+
+function calcolaFattoriale ($numero) {
+    return $numero--;
+   }
+   
+   $numero = 6;
+   $fattoriale = calcolaFattoriale ($numero)
+   echo "Il fattoriale di $numero è $fattoriale.";
+
+
+   
