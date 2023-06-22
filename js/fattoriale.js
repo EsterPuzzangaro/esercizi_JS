749210c5c1d17ffbe675f4024192a94375d842d7
EsterPuzzangaro <esterpuzzangaro@gmail.com>
2023-06-22 17:25:52 +0200

    Update fattoriale.js

--- js/fattoriale.js
+++ js/fattoriale.js
@@ -1,17 +1,1 @@
-$n=(value)
 
-if ($n > 0) {
- //scrivo la moltiplicazione
-}
-
-else {
-    if ($n = 0) {
-    console.log("il risultato è 1")
-    }
-
-    else{
-        //scrivo la moltiplicazione
-    }
-
-}
-    
